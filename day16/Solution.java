a5e8ced6ea8690afed7db52ba87b94e53065e6bd
leedooil <107297272+leedooil@users.noreply.github.com>
2023-02-15 09:09:52 +0900

    2023/02/15
    - 편지
    - 가장 큰 수 찾기
    - 문자열 계산하기
    - 배열의 유사도

---
+++ day16/Solution.java
@@ -0,0 +1,13 @@
+package day16;
+
+public class Solution {
+	public int solution(String message) {
+        int answer = 0;
+        
+        char[] charArr = message.toCharArray();
+        
+        answer = charArr.length * 2;
+        
+        return answer;
+    }
+}
